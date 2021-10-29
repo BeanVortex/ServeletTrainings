8286dfe6513741bb9fb2169b183c921e2e7de6c3
darkdeveloper <goldeagle00110011@outlook.com>
2021-10-29 21:44:33 +0330

    first commit

---
+++ src/main/java/com/example/demo/HelloServlet.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import java.io.*;
+
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet("/hello-servlet")
+public class HelloServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        int num1 = Integer.parseInt(request.getParameter("num1"));
+        int num2 = Integer.parseInt(request.getParameter("num2"));
+        int sum = num1 + num2;
+        PrintWriter out = response.getWriter();
+        out.println(sum);
+    }
+
+    public void destroy() {
+        System.out.println("destroy");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,22 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>JSP - Hello World</title>
+    </head>
+    <body>
+        <form action="hello-servlet">
+            <label>
+                number1
+                <input type="text" name="num1">
+            </label>
+            <br>
+
+            <label>
+                number2
+                <input type="text" name="num2">
+            </label>
+            <button type="submit">send</button>
+        </form>
+    </body>
+</html>
